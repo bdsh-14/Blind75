0fe38941df620a229e616d6665d56e05c2d7c29b
Bidisha Biswas <bdsh.14@gmail.com>
2022-05-14 11:27:20 -0600

    Update Lc33.py

--- Lc33.py
+++ Lc33.py
@@ -1,2 +1,29 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
 def searchInSortedArray(nums, target):
-    pass
+    left, right = 0, len(nums)-1
+
+    while left <= right:
+        mid = (left+right)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        # left sorted portion
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        else:
+            # right sorted portion
+            if nums[mid] <= target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+    return -1
+
+nums = [4,5,6,7,0,1,2]
+print(searchInSortedArray(nums, 0))
