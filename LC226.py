fd28ecade62f53da653ad7acc55cbab7f3938dbe
Bidisha Biswas <bdsh.14@gmail.com>
2022-05-15 09:41:11 -0600

    Create LC226.py

---
+++ LC226.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/invert-binary-tree/
+
+# Process: for every node, look at it's children and swap them, recursively run on left subtree and right subtree
+# DFS - both pre and post order
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.left(root.right)
+
+        return root
